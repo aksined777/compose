45fd223f68f74b020e93d2d111186840f464f06f
dkachurovskii <den@Dens-MacBook-Pro.local>
2024-05-16 14:34:40 +0300

    Card

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,21 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,32 +39,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                NewText()
+                Column {
+                    ListItem("Name", "Prof")
+                    ListItem("Name", "Prof")
+                    ListItem("Name", "Prof")
+                    ListItem("Name", "Prof")
+                    ListItem("Name", "Prof")
+                    ListItem("Name", "Prof")
+                }
+
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun NewText() {
-    Row(Modifier.fillMaxSize().background(Color.Gray), horizontalArrangement = Arrangement.SpaceEvenly
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(5.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
         ) {
-        Column(Modifier.background(Color.Red)
-            .fillMaxHeight(0.5f)
-            .fillMaxWidth(0.5f), // 50% screen
-            verticalArrangement = Arrangement.SpaceAround,
-            horizontalAlignment = Alignment.End){
-            Text(text = "OLOLO")
-            Text(text = "QAZ", )
-            Text(text = "QAZ", Modifier.background(Color.Cyan))
-            Text(text = "Madrid")
-        }
-        Column(Modifier.background(Color.Yellow)
-            .fillMaxWidth(1f)){
-            Text(text = "OLOLO")
-            Text(text = "QAZ", Modifier.background(Color.Gray))
-            Text(text = "Madrid")
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.background(Color.Yellow)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.islands),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding()
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+
+                Column(modifier = Modifier.padding(20.dp)) {
+                    Text(text = stringResource(id = R.string.app_name))
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+
         }
     }
 
