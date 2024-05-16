14812b887886f78ae9dbd7a48baa06bfa5d9efbd
dkachurovskii <den@Dens-MacBook-Pro.local>
2024-05-16 13:29:44 +0300

    Column

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,44 +4,55 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NewText()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+fun NewText() {
+    Row(Modifier.fillMaxSize().background(Color.Gray), horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+        Column(Modifier.background(Color.Red)
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth(0.5f), // 50% screen
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.End){
+            Text(text = "OLOLO")
+            Text(text = "QAZ", )
+            Text(text = "QAZ", Modifier.background(Color.Cyan))
+            Text(text = "Madrid")
+        }
+        Column(Modifier.background(Color.Yellow)
+            .fillMaxWidth(1f)){
+            Text(text = "OLOLO")
+            Text(text = "QAZ", Modifier.background(Color.Gray))
+            Text(text = "Madrid")
+        }
     }
+
 }
