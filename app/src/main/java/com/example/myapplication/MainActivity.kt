1554ff890e30fe4b701484ee2334ec2619bf59a6
dkachurovskii <den@Dens-MacBook-Pro.local>
2024-05-16 15:40:15 +0300

    Modifier click and view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,10 +19,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
@@ -27,7 +36,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,15 +48,33 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val context = LocalContext.current
             MyApplicationTheme {
-                Column {
-                    ListItem("Name", "Prof")
-                    ListItem("Name", "Prof")
-                    ListItem("Name", "Prof")
-                    ListItem("Name", "Prof")
-                    ListItem("Name", "Prof")
-                    ListItem("Name", "Prof")
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                        .offset(x = 50.dp, y = 90.dp)
+                ) {
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+                    ListItem("Name", "Prof", context)
+
                 }
 
             }
@@ -55,11 +84,22 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-private fun ListItem(name: String, prof: String) {
+private fun ListItem(name: String, prof: String, context: Context) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(16.dp)
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress { change, dragAmount ->
+                    Log.d("OLOLO", "Long press ${change.pressed}")
+                    Log.d("OLOLO", "Long press $dragAmount")
+                }
+            }
+            .clickable {
+                Toast
+                    .makeText(context, "&name $prof", Toast.LENGTH_SHORT)
+                    .show()
+            },
         shape = RoundedCornerShape(5.dp),
         elevation = CardDefaults.cardElevation(5.dp)
     ) {
