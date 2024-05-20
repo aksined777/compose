ba60b0984bf1bbe85cc32b12a81a522310b63487
dkachurovskii <den@Dens-MacBook-Pro.local>
2024-05-20 23:08:44 +0300

    create Card

--- app/src/main/java/com/example/myapplication/screen/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/screen/MainScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,6 +31,7 @@ import coil.compose.AsyncImage
 import com.example.myapplication.R
 import com.example.myapplication.ui.theme.ColorOLOLO
 import com.example.myapplication.ui.theme.Cyan100
+import java.nio.file.WatchEvent
 
 @Preview(showBackground = true)
 @Composable
@@ -61,21 +64,73 @@ fun MainScreen() {
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(
+                Column(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(PaddingValues(10.dp, 10.dp, 0.dp, 0.dp)),
+                            text = "20 Jun",
+                            style = TextStyle(fontSize = 15.sp),
+                            color = Color.White
+                        )
+
+                        AsyncImage(
+                            model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                            contentDescription = "im2",
+                            modifier = Modifier.size(35.dp)
+                        ) // coil library
+                    }
                     Text(
                         modifier = Modifier.padding(PaddingValues(10.dp, 10.dp, 0.dp, 0.dp)),
-                        text = "20 Jun",
-                        style = TextStyle(fontSize = 15.sp),
+                        text = "Oslo",
+                        style = TextStyle(fontSize = 24.sp),
                         color = Color.White
                     )
+                    Text(
+                        modifier = Modifier.padding(PaddingValues(10.dp, 10.dp, 0.dp, 0.dp)),
+                        text = "25 C",
+                        style = TextStyle(fontSize = 45.sp),
+                        color = Color.White
+                    )
+                    Text(
+                        modifier = Modifier.padding(PaddingValues(10.dp, 10.dp, 0.dp, 0.dp)),
+                        text = "Sunny",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.search_24),
+                                contentDescription = "Img3",
+                                tint = Color.White
+                            )
+                        }
+                        Text(
+                            modifier = Modifier.padding(PaddingValues(10.dp, 10.dp, 0.dp, 0.dp)),
+                            text = "23 C/12 C",
+                            style = TextStyle(fontSize = 16.sp),
+                            color = Color.White
+                        )
 
-                    AsyncImage(model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
-                        contentDescription = "im2",
-                        modifier = Modifier.size(35.dp)
-                        ) // coil library
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.cloud_sync_24),
+                                contentDescription = "Img4",
+                                tint = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -9,6 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-val Cyan100 = Color(0xFF03A9F4)
+val Cyan100 = Color(0xFF269BCF)
 
 val ColorOLOLO = Color(0xFFFF5722)
