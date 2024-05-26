69c24b0a184504f85270ee266957aa38c337bd57
dkachurovskii <den@Dens-MacBook-Pro.local>
2024-05-26 13:29:17 +0300

    get weather

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.myapplication.screen.MainCard
 import com.example.myapplication.screen.TabLayout
+import org.json.JSONObject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,5 +38,23 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
+
+private fun getData(name: String, context: Context, state: MutableState<String>) {
+    val API_KEY = "aa05fd44130742cf8b3181636241905"
+    val url = "https://api.weatherapi.com/v1/current.json" +
+            "?key=$API_KEY&" +
+            "q=$name" +
+            "&aqi=no"
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        { responce ->
+            val obj = JSONObject(responce)
+
+            state.value = obj.getJSONObject("current").getString("temp_c") },
+        { error -> state.value = error.message.toString()})
+    queue.add(stringRequest)
 }
